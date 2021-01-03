0c08366b49d6d187a68ef98e4f08d9cf3933fa33
annavasylashko <anna.vasylashko123@gmail.com>
2021-01-03 23:23:20 +0200

    Refactoring

--- task7/7.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-let upCase = String("ECMAScript 2015 (6th Edition, ECMA-262)".match(/[A-F]/g));
-// [A-F] -> Square brackets may contain character ranges. In this case, it searces for every uppercase letter.
-// /g -> With this flag, search looks for all matches. Without him - only the first.
-console.log(upCase);
